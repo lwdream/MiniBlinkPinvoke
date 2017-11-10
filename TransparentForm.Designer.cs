af3deab333024577a7723b6283a3256453adb8de
hejialong <E024@users.noreply.github.com>
2017-11-10 22:52:19 +0800

    添加无边框窗体，使用Transparent接口

---
+++ TransparentForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace MiniBlinkPinvoke
+{
+    partial class TransparentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.blinkBrowser1 = new MiniBlinkPinvoke.BlinkBrowser();
+            this.SuspendLayout();
+            // 
+            // blinkBrowser1
+            // 
+            this.blinkBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.blinkBrowser1.HTML = "";
+            this.blinkBrowser1.JsGetValue = null;
+            this.blinkBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.blinkBrowser1.Name = "blinkBrowser1";
+            this.blinkBrowser1.Size = new System.Drawing.Size(740, 627);
+            this.blinkBrowser1.TabIndex = 0;
+            this.blinkBrowser1.Text = "blinkBrowser1";
+            this.blinkBrowser1.Url = "";
+            // 
+            // TransparentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Magenta;
+            this.ClientSize = new System.Drawing.Size(740, 627);
+            this.Controls.Add(this.blinkBrowser1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "TransparentForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TransparentForm";
+            this.TransparencyKey = System.Drawing.Color.Magenta;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private BlinkBrowser blinkBrowser1;
+    }
+}
---
+++ TransparentForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MiniBlinkPinvoke
+{
+    public partial class TransparentForm : Form
+    {
+        public TransparentForm()
+        {
+            InitializeComponent();
+            blinkBrowser1.GlobalObjectJs = this;
+            Load += TransparentForm_Load;
+        }
+
+        private void TransparentForm_Load(object sender, EventArgs e)
+        {
+            if (blinkBrowser1.handle!=IntPtr.Zero)
+            {
+                blinkBrowser1.DocumentReadyCallback += BlinkBrowser1_DocumentReadyCallback;
+                BlinkBrowserPInvoke.wkeSetTransparent(blinkBrowser1.handle, true);
+                blinkBrowser1.Url = "file:///F:/Project/MiniBlinkPinvokeForGithub/MiniBlinkPinvoke/%E7%BA%AFCSS3%E5%AE%9E%E7%8E%B0%E8%8B%B9%E6%9E%9C%E8%AE%BE%E5%A4%87DEMO%E6%BC%94%E7%A4%BAiPhone/devices.html";
+              
+            }
+        }
+
+        private void BlinkBrowser1_DocumentReadyCallback()
+        {
+            blinkBrowser1.Invalidate();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        public static void MouseToMoveControl(IntPtr handle)
+        {
+            ReleaseCapture();
+            SendMessage(handle, 0x112, 0xf012, 0);
+        }
+
+        [JSFunctin]
+        public void MoveWindow()
+        {
+            MouseToMoveControl(base.Handle);
+        }
+    }
+}
